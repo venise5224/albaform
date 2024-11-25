7592433b843ba746b5c61901809f6508b56f0390
Wooy <wjddndud5023@gmail.com>
2024-11-25 17:17:47 +0900

    #21 Refactor: 중복 스타일, 컴포넌트 내에 변수로 선언

--- src/components/button/SolidButton.tsx
+++ src/components/button/SolidButton.tsx
@@ -19,35 +19,42 @@ const SolidButton = ({
   disabled = false,
   ...rest
 }: SolidButtonProps) => {
+  // 버튼 스타일 변수 선언
+  const styles = {
+    buttonOrange300: "bg-orange-300 text-white active:scale-95",
+    buttonOrange200: "bg-orange-200 text-white active:scale-95",
+    outButtonOrange300:
+      "border border-orange-300 bg-transparent text-orange-300 active:scale-95",
+    outButtonOrange200:
+      "border border-orange-200 bg-transparent text-orange-200 active:scale-95",
+    disabledButtonOrange: "bg-gray-100 text-white",
+    disabledOutButtonOrange:
+      "border border-gray-100 bg-transparent text-gray-100",
+  };
+
+  // 활성화 상태 or 비활성화 상태 스타일
+  const buttonStyles = {
+    orange300: disabled ? styles.disabledButtonOrange : styles.buttonOrange300,
+    orange200: disabled ? styles.disabledButtonOrange : styles.buttonOrange200,
+    outOrange300: disabled
+      ? styles.disabledOutButtonOrange
+      : styles.outButtonOrange300,
+    outOrange200: disabled
+      ? styles.disabledOutButtonOrange
+      : styles.outButtonOrange200,
+  };
+
   // 크기별 스타일
   const sizeStyle = {
     small: "w-full max-w-[327px] py-4 text-base",
     large: "w-full max-w-[640px] py-5 text-xl",
   };
 
-  // 활성화 상태 or 비활성화 상태 스타일
-  const buttonStyles = {
-    orange300: disabled
-      ? "solid-disabled-button-orange"
-      : "solid-button-orange",
-    orange200: disabled
-      ? "solid-disabled-button-orange"
-      : "solid-button-orange",
-    outOrange300: disabled
-      ? "solid-disabled-button-outOrange"
-      : "solid-button-outOrange",
-    outOrange200: disabled
-      ? "solid-disabled-button-outOrange"
-      : "solid-button-outOrange",
-  };
-
-  const styleClass = buttonStyles[style];
-
   // 공통
   const commonClass = `flex items-center gap-x-2 font-semibold rounded-lg justify-center transition-transform duration-200 ease-out hover:opacity-90}`;
 
   // 최종 스타일
-  const finalClassName = `${commonClass} ${sizeStyle[size]} ${styleClass}`;
+  const finalClassName = `${commonClass} ${sizeStyle[size]} ${buttonStyles[style]}`;
 
   return (
     <button className={finalClassName} disabled={disabled} {...rest}>
