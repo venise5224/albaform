e30437e201fc7d3e17cccf9991003d79d04de7d3
junjeeong <wn8624@naver.com>
2024-11-25 12:58:21 +0900

    Chore: 테스트 코드 삭제

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  images: {
-    domains: ["www.youtube.com"], // 허용할 도메인 추가
-  },
 };
 
 export default nextConfig;
