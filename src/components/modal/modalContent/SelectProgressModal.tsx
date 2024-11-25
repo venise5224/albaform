fefdf64e7412b3db5327aa9118dffb446f2124e9
JSM <venise5224@gmail.com>
2024-11-26 05:34:04 +0900

    진행상태 선택 모달 UI 구현

--- src/components/modal/modalContent/SelectProgressModal.tsx
+++ src/components/modal/modalContent/SelectProgressModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import ModalContainer from "../modalContainer/ModalContainer";
 
@@ -9,32 +11,45 @@ const progressList = [
 ];
 
 const SelectProgressModal = () => {
-  const [selected, setSelected] = useState(false);
+  const [selected, setSelected] = useState("");
+
+  const handleChange = (value: string) => {
+    setSelected(value);
+  };
 
   return (
     <ModalContainer>
-      <div className="flex flex-col items-center pb-[8px]">
+      <div className="flex flex-col items-center pb-[8px] pt-[4px] pc:mx-[-40px] pc:pb-[8px] pc:pt-[8px]">
         <strong className="modal-title mt-0">진행상태 선택</strong>
         <p className="modal-sub-title">현재 진행상태를 알려주세요.</p>
 
-        <ul className="mt-[24px] flex w-full flex-col gap-[10px]">
+        <ul className="mt-[24px] flex w-full flex-col gap-[10px] pc:mt-12">
           {progressList.map((progress) => (
             <li key={progress.name}>
-              <label className="flex justify-between rounded-[8px] border border-line-100 p-[14px] text-md font-medium">
+              <label
+                className={`bg-orang-100 flex items-center justify-between rounded-[8px] border border-line-100 p-[14px] text-md font-medium pc:text-2lg ${selected === progress.name ? "border-orange-300 bg-orange-500" : ""}`}
+              >
                 {progress.name}
                 <input
-                  onChange={() => setSelected(!selected)}
                   type="radio"
-                  className="w-[20px]"
+                  name="progress"
+                  value={progress.name}
+                  checked={selected === progress.name}
+                  onChange={() => handleChange(progress.name)}
+                  className="size-5 appearance-none rounded-full border-[5px] border-gray-50 ring-1 ring-gray-300 checked:bg-orange-300 checked:ring-orange-300"
                 />
               </label>
             </li>
           ))}
         </ul>
 
-        <div className="mt-[30px] flex gap-[11px]">
-          <button className="h-[58px] w-[158px] border">취소</button>
-          <button className="h-[58px] w-[158px] border">선택하기</button>
+        <div className="mt-[30px] flex gap-[11px] pc:gap-[8px]">
+          <button className="h-[58px] w-[158px] rounded-[8px] border bg-gray-100 text-white pc:h-[72px] pc:w-[176px]">
+            취소
+          </button>
+          <button className="h-[58px] w-[158px] rounded-[8px] border bg-orange-300 text-gray-50 pc:h-[72px] pc:w-[176px]">
+            선택하기
+          </button>
         </div>
       </div>
     </ModalContainer>
