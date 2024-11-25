773df43fe4af765cd53174d11d3e26fe96853f67
byeolee1221 <zicsso1221@gmail.com>
2024-11-25 19:50:49 +0900

    Chore: 최상단 layout에서 global.css 경로 원상복구된 것 다시 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import Header from "../components/header/Header";
 import Sidebar from "@/components/header/Sidebar";
 import ModalManager from "@/components/modal/modalManager/ModalManager";
