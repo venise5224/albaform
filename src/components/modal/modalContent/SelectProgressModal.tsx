2597be1a7b5d01a9ba6fe9fd35f8472fa2092104
JSM <venise5224@gmail.com>
2024-11-25 20:36:28 +0900

    Feat:SelectProgress모달 UI 구현

--- src/components/modal/modalContent/SelectProgressModal.tsx
+++ src/components/modal/modalContent/SelectProgressModal.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import ModalContainer from "../modalContainer/ModalContainer";
+
+const progressList = [
+  { name: "거절" },
+  { name: "면접 대기" },
+  { name: "면접 완료" },
+  { name: "채용 완료" },
+];
+
 const SelectProgressModal = () => {
-  return <div>진행상태 선택</div>;
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <ModalContainer>
+      <div className="flex flex-col items-center pb-[8px]">
+        <strong className="modal-title mt-0">진행상태 선택</strong>
+        <p className="modal-sub-title">현재 진행상태를 알려주세요.</p>
+
+        <ul className="mt-[24px] flex w-full flex-col gap-[10px]">
+          {progressList.map((progress) => (
+            <li key={progress.name}>
+              <label className="flex justify-between rounded-[8px] border border-line-100 p-[14px] text-md font-medium">
+                {progress.name}
+                <input
+                  onChange={() => setSelected(!selected)}
+                  type="radio"
+                  className="w-[20px]"
+                />
+              </label>
+            </li>
+          ))}
+        </ul>
+
+        <div className="mt-[30px] flex gap-[11px]">
+          <button className="h-[58px] w-[158px] border">취소</button>
+          <button className="h-[58px] w-[158px] border">선택하기</button>
+        </div>
+      </div>
+    </ModalContainer>
+  );
 };
 
 export default SelectProgressModal;
--- src/types/modal.tsx
+++ src/types/modal.tsx
@@ -2,4 +2,5 @@ export enum ModalType {
   ClosedAlbaformModal = "ClosedAlbaformModal", // 모집 마감 모달
   DeleteAlbaformModal = "DeleteAlbaformModal", // 알바폼 삭제 모달
   PatchAlbaformModal = "PatchAlbaformModal", // 알바폼 이어쓰기 모달
+  SelectProgressModal = "SelectProgressModal", // 진행상태 선택
 }
