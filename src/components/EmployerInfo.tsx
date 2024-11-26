3420c723ece19ad6d648ccb5424f877130c935a4
junjeeong <wn8624@naver.com>
2024-11-26 16:31:48 +0900

    Feat: EmployerInfo컴포넌트 구현

--- src/components/EmployerInfo.tsx
+++ src/components/EmployerInfo.tsx
@@ -1,3 +1,6 @@
+import { formatDate } from "@/utils/formatDate";
+import { getDday } from "@/utils/getDday";
+
 interface EmployerInfoProps {
   info: {
     recruitmentStartDate: string;
@@ -8,7 +11,38 @@ interface EmployerInfoProps {
 }
 
 const EmployerInfo = ({ info }: EmployerInfoProps) => {
-  return <div className></div>;
+  const dday = getDday(info.recruitmentEndDate);
+
+  const [formattedStartDay, formattedEndDay] = formatDate(
+    info.recruitmentStartDate,
+    info.recruitmentEndDate
+  );
+
+  const commonStyle =
+    "flex h-[96px] w-full items-center justify-between border-b border-b-line-100 text-xl text-black-100";
+
+  return (
+    <div className="flex h-[336px] w-[640px] items-center justify-center rounded-md border border-line-100 bg-background-100">
+      <div className="flex w-[592px] flex-col p-6">
+        <div className={commonStyle}>
+          <span>
+            모집기간 <b className="ml-4 text-orange-300">{dday}</b>
+          </span>
+          <span className="text-black-400">
+            {formattedStartDay} ~ {formattedEndDay}
+          </span>
+        </div>
+        <div className={commonStyle}>
+          <span>가게 전화번호</span>
+          <span className="text-black-400">{info.storePhoneNumber}</span>
+        </div>
+        <div className={commonStyle}>
+          <span>사장님 전화번호</span>
+          <span className="text-black-400">{info.phoneNumber}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployerInfo;
