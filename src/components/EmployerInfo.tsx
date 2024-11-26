f6fa447a09a14c463712152c1afbd0f3f38da17d
junjeeong <wn8624@naver.com>
2024-11-26 16:06:48 +0900

    Feat: EmployerInfo 컴포넌트 생성

---
+++ src/components/EmployerInfo.tsx
@@ -0,0 +1,14 @@
+interface EmployerInfoProps {
+  info: {
+    recruitmentStartDate: string;
+    recruitmentEndDate: string;
+    storePhoneNumber: string;
+    phoneNumber: string;
+  };
+}
+
+const EmployerInfo = ({ info }: EmployerInfoProps) => {
+  return <div className></div>;
+};
+
+export default EmployerInfo;
