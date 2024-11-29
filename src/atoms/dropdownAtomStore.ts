0c07cb8364d72dbf1af8f369132a53c3bdc8bf5c
byeolee1221 <zicsso1221@gmail.com>
2024-11-29 16:01:59 +0900

    Feat: 정보 수정 드롭다운 추가

--- src/atoms/dropdownAtomStore.ts
+++ src/atoms/dropdownAtomStore.ts
@@ -6,9 +6,6 @@ export const dropdownTriggerAtom = atomFamily((id: string) => atom(false));
 // 전체 - 모집 중 - 모집 마감 드롭다운 (isRecruiting)
 export const applicationStatusAtom = atom<boolean | undefined>(undefined);
 
-// 수정 및 삭제 드롭다운
-export const editDeleteDropdownAtom = atom(false);
-
 // 전체 - 공개 - 비공개 드롭다운 (isPublic)
 export const publicStatusAtom = atom<boolean | undefined>(undefined);
 
