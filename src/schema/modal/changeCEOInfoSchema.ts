e073b265044b2d9a205f817e48fc820ae6969e9c
JSM <venise5224@gmail.com>
2024-11-30 17:02:52 +0900

    Chore:스키마 수정

--- src/schema/modal/changeCEOInfoSchema.ts
+++ src/schema/modal/changeCEOInfoSchema.ts
@@ -4,20 +4,15 @@ export const changeCEOInfoSchema = z.object({
   nickname: z.string().min(1, { message: "닉네임을 입력해주세요." }),
   storeName: z
     .string()
-    .min(1, { message: "가게 이름(상호명)을 필수로 입력해주세요." })
-    .optional(),
+    .min(1, { message: "가게 이름(상호명)을 필수로 입력해주세요." }),
   storePhoneNumber: z
     .string()
-    .regex(/^[0-9]+$/, { message: "올바르지 않은 번호입니다." })
-    .optional(),
+    .regex(/^[0-9]+$/, { message: "올바르지 않은 번호입니다." }),
   phoneNumber: z
     .string()
-    .regex(/^(010|011|016|017|018|019)\d{7,8}$/, {
+    .regex(/^(|010|011|016|017|018|019)\d{7,8}$/, {
       message: "올바르지 않은 번호입니다.",
     })
-    .optional(),
-  location: z
-    .string()
-    .min(1, { message: "가게 위치를 입력해주세요." })
-    .optional(),
+    .nullish(),
+  location: z.string().min(1, { message: "가게 위치를 입력해주세요." }),
 });
