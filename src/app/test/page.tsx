169c7002dfee2b483b3223cf1dbc31ffbff4c2d8
junjeeong <wn8624@naver.com>
2024-12-02 03:14:57 +0900

    알바폼만들기 페이지 -> 모집 조건 페이지에  들어가는 공통 input 구현중

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import RequirementPicker from "../../components/picker/RequirementPicker";
+import RequirementPicker from "@/components/picker/RequirementPicker";
 
 const testPage = () => {
   return (
