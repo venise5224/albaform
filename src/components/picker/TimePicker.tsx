b59a1ff20fb317c73e9a825ab5b8d3a7b18ac884
junjeeong <wn8624@naver.com>
2024-12-03 03:46:34 +0900

    Feat: TimePicker 구현

--- src/components/picker/TimePicker.tsx
+++ src/components/picker/TimePicker.tsx
@@ -2,24 +2,30 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import PickableList from "./PickableList";
 
 const TimePicker = () => {
-  const [startTime, setStartTime] = useState("00:00");
-  const [endTime, setEndTime] = useState("24:00");
-  const [startOpen, setStartOpen] = useState(false);
-  const [endOpen, setEndOpen] = useState(false);
+  const [startTime, setStartTime] = useState({ time: "00:00", updated: false });
+  const [endTime, setEndTime] = useState({ time: "24:00", updated: false });
+  const [isStartButtonOpen, setIsStartButtonOpen] = useState(false);
+  const [isEndButtonOpen, setIsEndButtonOpen] = useState(false);
 
   return (
     <div className="flex h-[54px] w-[327px] gap-[27px] text-lg pc:h-[64px] pc:w-[456px] pc:gap-[36px] pc:text-xl">
+      {/* 시작시간 설정 버튼 */}
       <button
-        onClick={() => setStartOpen(!startOpen)}
-        className="flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-black-400"
+        onClick={() => setIsStartButtonOpen(!isStartButtonOpen)}
+        className={
+          startTime.updated
+            ? "relative flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-black-400"
+            : "relative flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-gray-400"
+        }
       >
         <Image src="/icon/clock-blank.svg" width={24} height={24} alt="" />
-        <span className="flex-grow">{startTime}</span>
+        <span className="flex-grow">{startTime.time}</span>
         <Image
           src={
-            startOpen
+            isStartButtonOpen
               ? "/icon/arrow-fill-top.svg"
               : "/icon/arrow-fill-bottom.svg"
           }
@@ -27,21 +33,43 @@ const TimePicker = () => {
           height={24}
           alt=""
         />
+        {isStartButtonOpen && (
+          <PickableList
+            label="근무시간"
+            setIsOpen={setIsStartButtonOpen}
+            setValue={setStartTime}
+          />
+        )}
       </button>
+
+      {/* 종료시간 설정 버튼 */}
       <button
-        onClick={() => setEndOpen(!endOpen)}
-        className="flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-black-400"
+        onClick={() => setIsEndButtonOpen(!isEndButtonOpen)}
+        className={
+          endTime.updated
+            ? "relative flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-black-400"
+            : "relative flex items-center gap-2 rounded-[8px] bg-background-200 p-[14px] text-gray-400"
+        }
       >
         <Image src="/icon/clock-blank.svg" width={24} height={24} alt="" />
-        <span className="flex-grow">{endTime}</span>
+        <span className="flex-grow">{endTime.time}</span>
         <Image
           src={
-            endOpen ? "/icon/arrow-fill-top.svg" : "/icon/arrow-fill-bottom.svg"
+            isEndButtonOpen
+              ? "/icon/arrow-fill-top.svg"
+              : "/icon/arrow-fill-bottom.svg"
           }
           width={24}
           height={24}
           alt=""
         />
+        {isEndButtonOpen && (
+          <PickableList
+            label="근무시간"
+            setIsOpen={setIsEndButtonOpen}
+            setValue={setEndTime}
+          />
+        )}
       </button>
     </div>
   );
