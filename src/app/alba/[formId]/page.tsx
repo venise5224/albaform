cc89df9d47c761cc75306db00ac8bab5ed609fe5
junjeeong <wn8624@naver.com>
2024-12-03 16:12:20 +0900

    Feat: layout 구성만 작업

---
+++ src/app/alba/[formId]/page.tsx
@@ -0,0 +1,12 @@
+const page = () => {
+  return (
+    <div className="felx-col pc:grid-row-2pc:gap-x-[150px] flex gap-[32px] pc:grid pc:grid-cols-2 pc:gap-y-[120px]">
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
+  );
+};
+
+export default page;
