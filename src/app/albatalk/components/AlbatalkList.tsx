8aa40a7f3b033db139468b32a250fdac5ce5573a
JSM <venise5224@gmail.com>
2024-12-03 16:30:06 +0900

    Feat:알바토크 페이지 구현

--- src/app/albatalk/components/AlbatalkList.tsx
+++ src/app/albatalk/components/AlbatalkList.tsx
@@ -5,20 +5,26 @@ import { PostCardProps } from "@/types/post";
 import { useEffect, useState } from "react";
 import { getArticles } from "../api/getArticles";
 import { useSearchParams } from "next/navigation";
+import { useAtomValue } from "jotai";
+import { albatalkFilterAtom } from "@/atoms/dropdownAtomStore";
+import useViewPort from "@/hooks/useViewport";
 
 const AlbatalkList = ({
   posts: initialList,
-  nextCursor,
+  nextCursor: initialNextCursor,
 }: {
   posts: PostCardProps[];
   nextCursor: number | null;
 }) => {
-  const [list, setList] = useState(initialList);
-  const [cursor, setCursor] = useState(nextCursor);
+  const [posts, setPosts] = useState(initialList);
+  const [cursor, setCursor] = useState(initialNextCursor);
   const [isLoading, setIsLoading] = useState(false);
+  const fillter = useAtomValue(albatalkFilterAtom);
 
-  const searchParams = useSearchParams(); // 쿼리스트링 값 가져오기
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword") || "";
 
+  //데이터 변경 시 요청 로직
   useEffect(() => {
     const fetchInitialData = async () => {
       setIsLoading(true);
@@ -26,9 +32,11 @@ const AlbatalkList = ({
         const response = await getArticles({
           limit: 6,
           cursor: 0,
+          keyword,
+          orderBy: fillter.value,
         });
 
-        setList(response.data);
+        setPosts(response.data);
         setCursor(response.nextCursor);
       } catch (error) {
         console.error("초기 데이터를 불러오는데 실패했습니다.", error);
@@ -38,32 +46,13 @@ const AlbatalkList = ({
     };
 
     fetchInitialData();
-  }, [searchParams]);
-
-  const fetchMoreData = async () => {
-    if (!cursor || isLoading) return;
-
-    setIsLoading(true);
-    try {
-      const response = await getArticles({
-        limit: 6,
-        cursor,
-      });
-
-      setList((prevList) => [...prevList, ...response.data]);
-      setCursor(response.nextCursor);
-    } catch (error) {
-      console.error("추가 데이터를 불러오는데 실패했습니다.", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  }, [searchParams, keyword, fillter]);
 
   return (
     <div>
-      {list.length > 0 ? (
+      {posts.length > 0 ? (
         <div className="grid grid-cols-1 gap-4 pc:grid-cols-3 pc:gap-x-[25px] pc:gap-y-[48px]">
-          {list.map((post) => (
+          {posts.map((post) => (
             <PostCard key={post.id} info={post} />
           ))}
         </div>
