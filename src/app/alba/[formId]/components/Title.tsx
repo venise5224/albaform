64874348ce1c5a42f237cbf59dae7de0ff8542e3
junjeeong <wn8624@naver.com>
2024-12-04 18:28:07 +0900

    Feat: 데이터 패팅까지 완료, StoreLocation 컴포넌트 만들어야되고 carousel 구현해야 함

--- src/app/alba/[formId]/components/Title.tsx
+++ src/app/alba/[formId]/components/Title.tsx
@@ -20,7 +20,7 @@ const Title = ({ info }: { info: titleData }) => {
   const submitDate = getCurrentTime(info.createdAt);
 
   return (
-    <section className="flex flex-col gap-6">
+    <section className="flex w-[327px] flex-col gap-6 pc:w-[720px]">
       <div className="flex items-center justify-between text-xs pc:justify-start pc:text-md">
         <ul className="flex gap-2">
           <li className="rounded-md border border-orange-100 bg-orange-50 px-2 py-1 font-semibold text-orange-300">
--- src/app/alba/[formId]/page.tsx
+++ src/app/alba/[formId]/page.tsx
@@ -1,34 +1,50 @@
-import { mock } from "./mock";
 import SimpleRequirements from "@/components/card/SimpleRequirements";
 import Content from "./components/Content";
 import Title from "./components/Title";
 import EmployerInfo from "@/components/card/EmployerInfo";
 import DetailRequirements from "@/components/card/DetailRequirements";
 import SolidButton from "../../../components/button/SolidButton";
+import fetchData from "./api/fetchData";
 
-const AlbarformDetailPage = async () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+interface AlbarformDetailPageProps {
+  params: {
+    formId: string;
+  };
+}
 
-  if (apiUrl) {
-    const data = await fetch(apiUrl).then((res) => res.json());
+const AlbarformDetailPage = async ({ params }: AlbarformDetailPageProps) => {
+  const { formId } = params;
+  let data;
+
+  try {
+    data = await fetchData(formId);
+  } catch (error) {
+    console.error(error); // toast로 error 메시지 띄우는 걸로 바꿔야 함.
+
+    return (
+      <div>
+        <h1>데이터를 불러올 수 없습니다.</h1>
+        <p>잠시 후 다시 시도해주세요.</p>
+      </div>
+    );
   }
 
   return (
-    <div className="grid gap-[32px] pc:grid-cols-2 pc:justify-items-center pc:gap-0 pc:gap-x-[150px] pc:gap-y-[40px] pc:grid-areas-layout tablet:w-[327px] tablet:grid-cols-1 tablet:grid-rows-[270px_116px_156px_396px_302px_340px_158px] mobile:w-[327px] mobile:grid-cols-1 mobile:grid-rows-[270px_116px_156px_396px_302px_340px_158px]">
+    <div className="grid gap-[32px] pc:grid-cols-[770px_640px] pc:justify-items-center pc:gap-0 pc:gap-x-[150px] pc:gap-y-[40px] pc:grid-areas-layout tablet:w-[327px] tablet:grid-cols-1 tablet:grid-rows-[270px_116px_156px_396px_302px_340px_158px] mobile:w-[327px] mobile:grid-cols-1 mobile:grid-rows-[270px_116px_156px_396px_302px_340px_158px]">
       <div className="pc:grid-in-box1">
-        <Title info={mock} />
+        <Title info={data} />
       </div>
       <div className="pc:grid-in-box4">
-        <SimpleRequirements info={mock} />
+        <SimpleRequirements info={data} />
       </div>
       <div className="pc:grid-in-box5">
-        <EmployerInfo info={mock} />
+        <EmployerInfo info={data} />
       </div>
       <div className="pc:grid-in-box2">
-        <Content description={mock.description} />
+        <Content description={data.description} />
       </div>
       <div className="pc:grid-in-box7">
-        <DetailRequirements info={mock} />
+        <DetailRequirements info={data} />
       </div>
       {/* <StoreLocation/> */}
       <div className="pc:grid-in-box6">
