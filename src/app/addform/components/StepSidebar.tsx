a403dee6f4e1d7159a95df32b43a7cfcad584b0a
byeolee1221 <zicsso1221@gmail.com>
2024-12-05 00:10:16 +0900

    Feat: 임시저장 기능 추가

--- src/app/addform/components/StepSidebar.tsx
+++ src/app/addform/components/StepSidebar.tsx
@@ -1,12 +1,41 @@
 "use client";
 
 import StepButton from "./StepButton";
+import { AddFormStepProps } from "@/types/addform";
 
-const StepSidebar = () => {
+interface StepSidebarProps {
+  temporaryDataByStep: AddFormStepProps;
+  step: string;
+}
+
+const StepSidebar = ({ temporaryDataByStep, step }: StepSidebarProps) => {
+  const temporaryDataArr = [
+    { step: "stepOne", data: temporaryDataByStep.stepOne },
+    { step: "stepTwo", data: temporaryDataByStep.stepTwo },
+    { step: "stepThree", data: temporaryDataByStep.stepThree },
+  ];
+
+  const handleTemporarySave = () => {
+    temporaryDataArr.forEach((item) => {
+      localStorage.setItem(item.step, JSON.stringify(item.data));
+    });
+  };
+
   return (
     <div className="hidden h-[966px] w-[452px] flex-col justify-between rounded-3xl bg-background-200 p-8 pc:flex">
       <div className="flex flex-col space-y-2">
         <StepButton />
+      </div>
+      <div className="flex flex-col space-y-2">
+        <button
+          onClick={handleTemporarySave}
+          className="rounded-lg border-[1px] border-gray-200 p-4 text-xl font-semibold text-gray-300 transition-all hover:border-orange-300 hover:bg-background-100 hover:text-orange-300"
+        >
+          임시 저장
+        </button>
+        <button className="rounded-lg bg-gray-100 p-4 text-xl font-semibold text-white transition-all hover:bg-orange-300">
+          등록하기
+        </button>
       </div>
     </div>
   );
--- src/app/addform/page.tsx
+++ src/app/addform/page.tsx
@@ -1,9 +1,46 @@
+import { AddFormStepProps } from "@/types/addform";
 import StepSidebar from "./components/StepSidebar";
 
-const AddFormPage = async () => {
+interface AddFormPageProps {
+  searchParams: Promise<{ step: string }>;
+}
+
+const mockTemporaryDataByStep: AddFormStepProps = {
+  stepOne: {
+    title: "테스트 타이틀",
+    description: "테스트 설명",
+    recruitmentStartDate: "2024-12-04",
+    recruitmentEndDate: "2024-12-05",
+    imageUrls: [
+      "https://i.namu.wiki/i/TLYZbmNFPJa1hRl3A_O1MkTsy2R4FC13SSKN3hTMVAAPtVXEAc02vk6feRp3yntMLSxK9Ei2gsCXVRvyuKP0Ti3snEudeAvNMsDOf-b0HCNhBgqrqPXDvRyvDGAsMN-to26YD2vNX-BfQ6PaHrLxHA.webp",
+    ],
+  },
+  stepTwo: {
+    numberOfPositions: 1,
+    gender: "남성",
+    education: "고등학교 졸업",
+    age: "20대",
+    preferred: "성실성, 끈기",
+  },
+  stepThree: {
+    location: "광주광역시",
+    workStartDate: "2024-12-06",
+    workEndDate: "2024-12-07",
+    workStartTime: "09:00",
+    workEndTime: "18:00",
+    workDays: ["월", "화", "수", "목", "금"],
+    isNegotiableWorkDays: false,
+    hourlyWage: 9620,
+    isPublic: true,
+  },
+};
+
+const AddFormPage = async ({ searchParams }: AddFormPageProps) => {
+  const { step } = await searchParams;
+  console.log(step);
   return (
     <>
-      <StepSidebar />
+      <StepSidebar temporaryDataByStep={mockTemporaryDataByStep} step={step} />
     </>
   );
 };
