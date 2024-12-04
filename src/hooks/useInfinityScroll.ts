030edec677a849aaac4223959c8446e486ad74e6
JSM <venise5224@gmail.com>
2024-12-04 15:17:55 +0900

    Feat:무한스크롤 구현 중

--- src/hooks/useInfinityScroll.ts
+++ src/hooks/useInfinityScroll.ts
@@ -1,48 +1,15 @@
 import { ArticleData } from "@/types/article";
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 
-interface FetchMoreDataProps {
-  articles: ArticleData[];
-  nextCursor: number | null;
-  fetchMoreData: {
-    data: ArticleData[];
-    nextCursor: number | null;
-  };
-}
-
-const useInfiniteScroll = <T>(
-  fetchMoreData: (cursor: number | null) => Promise<FetchMoreDataProps<T>>,
-  initialData: T[],
-  initialCursor: string | null
-) => {
-  const [data, setData] = useState<T[]>(initialData);
-  const [nextCursor, setNextCursor] = useState<string | null>(initialCursor);
+const useInfiniteScroll = () => {
   const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(!!initialCursor);
+  const [hasMore, setHasMore] = useState();
   const observerRef = useRef<HTMLDivElement | null>(null);
-
-  const loadMore = useCallback(async () => {
-    if (!hasMore || loading || !nextCursor) return;
-
-    setLoading(true);
-    try {
-      const { data: newData, nextCursor: newCursor } =
-        await fetchMoreData(newCursor);
-      setData((prev) => [...prev, ...newData]);
-      setNextCursor(newCursor);
-      setHasMore(!!newCursor);
-    } catch (error) {
-      console.error("Failed to fetch more data:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, [fetchMoreData, hasMore, loading, nextCursor]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && hasMore && !loading) {
-          loadMore();
         }
       },
       { rootMargin: "100px" } // 여유 공간 설정
@@ -54,9 +21,9 @@ const useInfiniteScroll = <T>(
     return () => {
       if (currentObserver) observer.unobserve(currentObserver);
     };
-  }, [loadMore, hasMore, loading]);
+  }, [hasMore, loading]);
 
-  return { data, loading, hasMore, observerRef };
+  return { loading, hasMore, observerRef };
 };
 
 export default useInfiniteScroll;
