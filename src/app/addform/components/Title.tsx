b31a3377c268b5ac10c7b5275c4da4e3943ad246
byeolee1221 <zicsso1221@gmail.com>
2024-12-05 20:41:39 +0900

    Chore: 이전페이지로 돌아갈 때 모달경고창 대신 confirm으로 확인

--- src/app/addform/components/Title.tsx
+++ src/app/addform/components/Title.tsx
@@ -5,13 +5,20 @@ import { useRouter } from "next/navigation";
 const Title = () => {
   const router = useRouter();
 
+  const handleCancel = () => {
+    const isConfirm = confirm("작성을 취소하시겠습니까?"); // 모달 변경 필요
+    if (isConfirm) {
+      router.push("/albalist/owner");
+    }
+  };
+
   return (
     <div className="flex w-full items-center justify-between p-6 pc:m-0 pc:w-[640px]">
       <h2 className="text-xl font-semibold text-black-500 pc:text-3xl">
         알바폼 만들기
       </h2>
       <button
-        onClick={() => router.push("/albalist/owner")}
+        onClick={handleCancel}
         className="rounded-lg bg-gray-100 px-3.5 py-2 text-md font-semibold text-white transition-colors hover:bg-orange-300 pc:text-xl"
       >
         작성 취소
