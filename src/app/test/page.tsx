cacbb44075f07a74db86330be8f4ee4ec3458b9c
byeolee1221 <zicsso1221@gmail.com>
2024-12-08 22:29:19 +0900

    Chore: test 페이지 원래상태 복구

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import StoreLocation from "@/components/card/StoreLocation";
-import DatePickerCalendar from "@/components/picker/DatepickerCalendar";
 
 const page = () => {
   const location = "서울시 경인로 20가길 11";
-  return <DatePickerCalendar />;
+  return <StoreLocation location={location} />;
 };
 
 export default page;
