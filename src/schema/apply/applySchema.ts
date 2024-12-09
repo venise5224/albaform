2cbefdc0b52e0fcf50e04f49c40a092ec0179225
JSM <venise5224@gmail.com>
2024-12-09 15:23:15 +0900

    Chore:벨리데이션 수정

--- src/schema/apply/applySchema.ts
+++ src/schema/apply/applySchema.ts
@@ -5,20 +5,27 @@ export const applySchema = z.object({
   phoneNumber: z.string().regex(/^(|010|011|016|017|018|019)\d{7,8}$/, {
     message: "올바르지 않은 번호입니다.",
   }),
-  experienceMonths: z.string(),
-  resumeName: z.string(),
+  experienceMonths: z
+    .string()
+    .regex(/^\d+$/, { message: "숫자(정수)만 입력해주세요." })
+    .refine((value) => parseInt(value, 10) <= 600, {
+      message: "최대 600개월까지 입력 가능합니다.",
+    }),
+
+  resumeName: z.string().regex(/\.(pdf|word)$/, {
+    message: "허용되는 파일 형식은 pdf, word 입니다.",
+  }),
   introduction: z
     .string()
     .min(1, { message: "자기 소개를 입력해주세요." })
-    .max(200, { message: "200자 이내로 입력해주세요." }),
+    .max(200, { message: "200자 이내로 입력해주세요." })
+    .regex(/([A-Za-zㄱ-ㅎ가-힣0-9!@#$%^&*()_+={}\[\]:;"'<>,.?\/`~\\|-])/, {
+      message: "해당 기호는 사용할 수 없습니다.",
+    })
+    .trim(),
   password: z
     .string()
+    .min(4, { message: "비밀번호 4자 이상 입력해주세요." })
     .max(12, { message: "비밀번호는 12자 이내여야합니다." })
-    .regex(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!\"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]).{7,}$/,
-      {
-        message: "특수문자, 문자, 숫자 포함 7자 이상 입력해주세요.",
-      }
-    )
     .trim(),
 });
