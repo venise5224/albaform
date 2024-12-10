69509968e87a796cd61b98257d6472bdef91ebcd
byeolee1221 <zicsso1221@gmail.com>
2024-12-10 19:12:31 +0900

    Chore: 임시저장버튼 활용하는 쪽으로 재수정

--- src/app/addform/components/MainButton.tsx
+++ src/app/addform/components/MainButton.tsx
@@ -3,32 +3,25 @@
 import { temporaryDataByStepAtom } from "@/atoms/addFormAtom";
 import SolidButton from "@/components/button/SolidButton";
 import { useAtomValue } from "jotai";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 
 // 추후 form 작업을 하며 zod 타입, react-hook-form 타입이 추가될 예정입니다.
 const MainButton = () => {
   const temporaryDataByStep = useAtomValue(temporaryDataByStepAtom);
-  const temporaryDataArr = useMemo(
-    () => [
-      { step: "stepOne", data: temporaryDataByStep.stepOne },
-      { step: "stepTwo", data: temporaryDataByStep.stepTwo },
-      { step: "stepThree", data: temporaryDataByStep.stepThree },
-    ],
-    [temporaryDataByStep]
-  );
+  const temporaryDataArr = [
+    { step: "stepOne", data: temporaryDataByStep.stepOne },
+    { step: "stepTwo", data: temporaryDataByStep.stepTwo },
+    { step: "stepThree", data: temporaryDataByStep.stepThree },
+  ];
 
-  // 임시 저장 기능 (작성한만큼 로컬스토리지에 저장됩니다.)
-  const handleTemporarySave = useCallback(() => {
+  // 임시 저장 기능 (현재는 단계별로 임시저장이 실행되어야 로컬스토리지에 저장됩니다.)
+  const handleTemporarySave = () => {
     temporaryDataArr.forEach((item) => {
       if (item.data && Object.keys(item.data).length > 0) {
         localStorage.setItem(item.step, JSON.stringify(item.data));
       }
     });
-  }, [temporaryDataArr]);
-
-  useEffect(() => {
-    handleTemporarySave();
-  }, [handleTemporarySave]);
+  };
 
   const onSubmit = () => {
     // 성공 시 임시데이터 삭제
