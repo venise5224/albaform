ac952f8893fd476295a36f8d7b4ddd7d10f45428
JSM <venise5224@gmail.com>
2024-12-13 15:44:40 +0900

    Chore:빌드 오류 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useModal } from "@/hooks/useModal";
-import ScrapAndShareButton from "../alba/[formId]/components/ScrapAndShareButton";
 
 const Page = () => {
   const { openModal } = useModal();
 
   return (
     <div>
-      <ScrapAndShareButton isScrapped={false} formId={3} />;
       <div>
         <button onClick={() => openModal("SelectLocationModal")}>
           위치 선택 모달
