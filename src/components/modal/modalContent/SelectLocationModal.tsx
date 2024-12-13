258124a508269246b166b6eb44b5ea32298b286c
JSM <venise5224@gmail.com>
2024-12-13 15:02:14 +0900

    Feat:근무위치 입력 기능 구현

--- src/components/modal/modalContent/SelectLocationModal.tsx
+++ src/components/modal/modalContent/SelectLocationModal.tsx
@@ -1,42 +1,58 @@
 import SolidButton from "@/components/button/SolidButton";
 import ModalContainer from "../modalContainer/ModalContainer";
-import Image from "next/image";
 import { useModal } from "@/hooks/useModal";
-import useViewPort from "@/hooks/useViewport";
+import { useState } from "react";
+import SearchLocation from "@/components/input/SearchLocation";
+import KakaoMap2 from "@/components/map/KakaoMap2";
 import KakaoMap from "@/components/map/KakaoMap";
-import SearchInput from "@/components/input/SearchInput";
+import { useAtom } from "jotai";
+import { addressAtom } from "@/atoms/addressAtom";
+import { useToast } from "@/hooks/useToast";
 
 const SelectLocationModal = () => {
+  const { addToast } = useToast();
   const { closeModal } = useModal();
-  const viewPort = useViewPort();
+  const [location, setLocation] = useState<string>("");
+  const [address, setAddress] = useAtom(addressAtom);
 
-  const handleSubmit = () => {
-    //카카오맵에서 선택한 위치 게시
+  const handleSearch = (search: string) => {
+    setLocation(search);
   };
 
-  const location = "서울시 양천구 신월동";
+  const handleLocationFound = (newAddress: string) => {
+    setAddress(newAddress);
+  };
+
+  const handleSubmit = () => {
+    addToast("근무지가 입력되었습니다", "success");
+    closeModal();
+  };
 
   return (
     <ModalContainer>
-      <div className="flex flex-col items-center pb-[8px]">
-        <strong className="text-2lg font-semibold text-black-400 pc:text-[32px] pc:leading-[46px]">
+      <div className="flex flex-col items-center pb-[8px] pc:max-w-[720px]">
+        <h2 className="text-2lg font-semibold text-black-400 pc:text-[32px] pc:leading-[46px]">
           근무지역 선택
-        </strong>
-
-        <form onSubmit={handleSubmit} className="mt-6 flex flex-col pc:mt-10">
-          <div className="mt-6 flex flex-col gap-[11px] pc:mt-[30px] pc:gap-3">
-            <div className="flex gap-3">
-              <SearchInput placeholder="근무지를 입력해주세요" />
-              <div className="w-[80px]">
-                <SolidButton style="orange300">검색</SolidButton>
-              </div>
-            </div>
-            <div className="mt-2 h-[280px] w-full pc:mt-12 pc:h-[380px] pc:px-0">
-              <KakaoMap location={location} />
+        </h2>
+        <div className="mt-6 flex w-full gap-3">
+          <SearchLocation onSearch={handleSearch} />
+          <div className="w-[80px] pc:h-[62px] pc:w-[200px]">
+            <SolidButton style="orange300">검색</SolidButton>
+          </div>
+        </div>
+        <form className="flex w-full flex-col">
+          <div className="mt-6 flex flex-col gap-6">
+            <div className="h-[280px] w-full pc:h-[380px] pc:px-0">
+              <KakaoMap2
+                location={location}
+                onLocationFound={handleLocationFound}
+                clickEnabled
+              />
+              {/* <KakaoMap location={location} /> */}
+              <p>{address}</p>
             </div>
             <div className="flex gap-3">
               <SolidButton
-                size={viewPort === "pc" ? "large" : "small"}
                 style="gray100"
                 type="button"
                 onClick={() => {
@@ -47,12 +63,9 @@ const SelectLocationModal = () => {
               </SolidButton>
 
               <SolidButton
-                size={viewPort === "pc" ? "large" : "small"}
                 style="orange300"
                 type="submit"
-                onClick={() => {
-                  closeModal();
-                }}
+                onClick={handleSubmit}
               >
                 게시하기
               </SolidButton>
@@ -65,5 +78,3 @@ const SelectLocationModal = () => {
 };
 
 export default SelectLocationModal;
-
-const buttmonContainerStyle = "h-[58px] w-[158px] pc:h-[72px] pc:w-[314px]";
