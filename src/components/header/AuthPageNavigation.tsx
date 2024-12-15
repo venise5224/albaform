bb4a12aebce93108dbb485a0f96c08d4a44458a3
junjeeong <wn8624@naver.com>
2024-12-15 12:33:22 +0900

    feat: 로그인버튼, 헤더 네비게이션 구현

--- src/components/header/AuthPageNavigation.tsx
+++ src/components/header/AuthPageNavigation.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const AuthPageNavigation = () => {
+  const pathname = usePathname();
+  const isSignInPage = pathname.includes("/signin");
+  const isApplicantPage =
+    pathname === "/signin/applicant" || pathname === "/signup/applicant";
+  const isOwnerPage =
+    pathname === "/signin/owner" || pathname === "/signup/owner";
+
   return (
     <ul className="flex gap-[24px] text-gray-300">
-      <li>사장님 전용</li>
-      <li>지원자 전용</li>
+      <li>
+        <Link
+          href={isSignInPage ? "/signin/owner" : "/signup/owner"}
+          className={isOwnerPage ? "text-orange-300" : ""}
+        >
+          사장님 전용
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={isSignInPage ? "/signin/applicant" : "/signup/applicant"}
+          className={isApplicantPage ? "text-orange-300" : ""}
+        >
+          지원자 전용
+        </Link>
+      </li>
     </ul>
   );
 };
