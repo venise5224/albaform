e0acdcc482d47d5485e75f52e0c196ce5f98066a
byeolee1221 <zicsso1221@gmail.com>
2024-12-17 18:32:35 +0900

    Chore: 리프레시토큰 받아 재요청 안되던 문제 해결

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -1,24 +1,59 @@
 "use server";
 
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
+import { deleteCookie } from "./cookie";
 
 // 토큰이 필요한 요청을 할 때 fetch 대신 instance를 사용합니다.
 const instance = async (url: string, options: RequestInit = {}) => {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
-  const instanceHeader = new Headers(options.headers || {});
-  instanceHeader.set("x-instance-request", "true");
-  console.log(instanceHeader);
+
   if (accessToken) {
-    instanceHeader.set("Authorization", `Bearer ${accessToken}`);
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${accessToken}`,
+    };
   }
 
-  const headerOptions = {
-    ...options,
-    headers: instanceHeader,
-  };
+  let response = await fetch(url, { ...options });
 
-  let response = await fetch(url, headerOptions);
+  if (response.status === 401) {
+    const refreshToken = cookieStore.get("refreshToken")?.value;
+
+    if (refreshToken) {
+      const refreshResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+        {
+          method: "POST",
+          body: JSON.stringify({ refreshToken }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (refreshResponse.ok) {
+        const tokenData = await refreshResponse.json();
+        const { accessToken: newAccessToken } = tokenData;
+
+        cookieStore.set("accessToken", newAccessToken);
+
+        options.headers = {
+          ...options.headers,
+          Authorization: `Bearer ${newAccessToken}`,
+        };
+
+        response = await fetch(url, { ...options });
+      } else {
+        console.error("토큰갱신 실패");
+        await deleteCookie(true);
+        return {
+          status: 401,
+          error: "장시간 미활동으로 인해 로그인이 해제되었습니다.",
+        };
+      }
+    }
+  }
 
   if (response.status === 204) {
     return {
@@ -35,9 +70,11 @@ const instance = async (url: string, options: RequestInit = {}) => {
     };
   }
 
+  const responseData = await response.json();
+
   return {
     status: response.status,
-    data: await response.json(),
+    ...responseData,
   }; // 각 요청의 응답을 json으로 반환합니다.
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,109 +8,31 @@ export const middleware = async (request: NextRequest) => {
   const ownerPath = ["/owner", "/addform", "/addform/", "/applications/"];
   const applicantPath = ["/applicant", "/apply/", "/myapply/"];
   const accessToken = request.cookies.get("accessToken");
-  const instanceRequest = request.headers.get("x-instance-request") === "true";
-  console.log(instanceRequest);
-
-  // instance로 요청이 들어왔을 때의 토큰 갱신 및 재요청
-  if (instanceRequest) {
-    console.log("instanceRequest");
-    if (!accessToken) {
-      return NextResponse.json(
-        { error: "로그인이 필요한 서버스입니다." },
-        { status: 401 }
-      );
-    }
-
-    try {
-      const response = await fetch(request);
-      const cookieList = ["accessToken", "refreshToken", "role", "id"];
-
-      if (response.status === 401) {
-        const refreshToken = request.cookies.get("refreshToken")?.value;
-
-        if (!refreshToken) {
-          const response = NextResponse.json(
-            { error: "장시간 미활동으로 인해 로그인이 해제되었습니다." },
-            { status: 401 }
-          );
-
-          cookieList.forEach((cookie) => {
-            response.cookies.delete(cookie);
-          });
-
-          return response;
-        }
-
-        const refreshResponse = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
-          {
-            method: "POST",
-            body: JSON.stringify({ refreshToken }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        console.log(refreshResponse);
-        if (refreshResponse.ok) {
-          const tokenData = await refreshResponse.json();
-          const { accessToken: newAccessToken } = tokenData;
-
-          const response = NextResponse.next();
-          response.cookies.set({
-            name: "accessToken",
-            value: newAccessToken,
-          });
-
-          const newRequest = new Request(request.url, {
-            method: request.method,
-            headers: new Headers(request.headers),
-            body: request.body,
-          });
-          newRequest.headers.set("Authorization", `Bearer ${newAccessToken}`);
-
-          const retryResponse = await fetch(newRequest);
-          const responseResult = NextResponse.json(await retryResponse.json(), {
-            status: retryResponse.status,
-          });
-
-          return responseResult;
-        } else {
-          const response = NextResponse.redirect(
-            new URL("/signin", request.url)
-          );
-
-          cookieList.forEach((cookie) => {
-            response.cookies.delete(cookie);
-          });
-
-          return response;
-        }
-      }
-    } catch (error) {
-      return NextResponse.json(
-        { error: "오류가 발생하여 요청이 실패했습니다." },
-        { status: 500 }
-      );
-    }
-  }
 
   // 사장님용 페이지
   if (role && role.value === "APPLICANT" && ownerPath.includes(path)) {
-    const prevUrl = referer || url.origin;
-    const errorUrl = new URL(prevUrl);
-    errorUrl.searchParams.set("error", "matchErrror");
+    // const prevUrl = referer || url.origin;
+    // const errorUrl = new URL(prevUrl);
+    // errorUrl.searchParams.set("error", "matchErrror");
 
-    return NextResponse.redirect(errorUrl);
+    if (referer) {
+      return NextResponse.redirect(referer);
+    } else {
+      return NextResponse.redirect(url.origin);
+    }
   }
 
   // 지원자용 페이지
   if (role && role.value === "OWNER" && applicantPath.includes(path)) {
-    const prevUrl = referer || url.origin;
-    const errorUrl = new URL(prevUrl);
-    errorUrl.searchParams.set("error", "matchErrror");
+    // const prevUrl = referer || url.origin;
+    // const errorUrl = new URL(prevUrl);
+    // errorUrl.searchParams.set("error", "matchErrror");
 
-    return NextResponse.redirect(errorUrl);
+    if (referer) {
+      return NextResponse.redirect(referer);
+    } else {
+      return NextResponse.redirect(url.origin);
+    }
   }
 
   // 로그인되어 있으면 로그인페이지로 못가도록 막음
