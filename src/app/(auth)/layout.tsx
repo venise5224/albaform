0c56409981b93108d3aa1dfffedf9391264631da
byeolee1221 <zicsso1221@gmail.com>
2024-12-17 23:30:32 +0900

    Chore: 태블릿 이하 버튼크기에 맞춰 max-width 적용, 어색하게 잘리는 문구 break-keep 적용

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto mb-12 mt-[94px] flex max-w-[640px] flex-col space-y-8 px-6">
+    <div className="mx-auto mb-12 mt-[94px] flex max-w-[327px] flex-col space-y-8 px-6 pc:max-w-[640px]">
       {children}
     </div>
   );
