ea95540c640213228764b525373a315ca1c2efa2
junjeeong <wn8624@naver.com>
2024-12-17 16:30:04 +0900

    작업중: SimpleRequirements 크기 늘려야 함

--- src/app/alba/[formId]/components/Content.tsx
+++ src/app/alba/[formId]/components/Content.tsx
@@ -1,6 +1,6 @@
 const Content = ({ description }: { description: string }) => {
   return (
-    <p className="px-6 py-4 text-md text-black-400 pc:text-2xl">
+    <p className="px-6 py-4 text-md text-black-400 pc:text-2xl tablet:text-lg">
       {description}
     </p>
   );
