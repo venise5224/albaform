dc3340199b27af9d6be584ba4ba0293b937a435c
Wooy <wjddndud5023@gmail.com>
2024-12-19 16:51:20 +0900

    AlbaFormList AlbarPreview role prop 추가

--- src/app/myalbaform/components/AlbaFormList.tsx
+++ src/app/myalbaform/components/AlbaFormList.tsx
@@ -77,12 +77,12 @@ const AlbaFormList = ({ list, nextCursor, role, params }: Props) => {
                 {item.isPublic ? (
                   <Link href={`/alba/${item.id}`} className="block">
                     <BlurWrapper isPublic={item.isPublic}>
-                      <AlbarPreview info={item} />
+                      <AlbarPreview info={item} role={role} />
                     </BlurWrapper>
                   </Link>
                 ) : (
                   <BlurWrapper isPublic={item.isPublic}>
-                    <AlbarPreview info={item} />
+                    <AlbarPreview info={item} role={role} />
                   </BlurWrapper>
                 )}
               </li>
