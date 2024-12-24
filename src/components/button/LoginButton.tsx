18cbe26a1a62d0f3e4fcdb357dab90de80013e62
junjeeong <wn8624@naver.com>
2024-12-25 02:37:12 +0900

    Fix: 로그인버튼이 햄버거로 바로 바뀌지 않는 버그 개선 + 사이드바 마이페이지로 이동시 Sidebar 닫히게 함

--- src/components/button/LoginButton.tsx
+++ src/components/button/LoginButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const LoginButton = () => {
   return (
     <Link href="/signin/applicant">
-      <div className="flex h-[52px] max-w-[120px] items-center justify-center rounded-lg text-lg text-orange-300 hover:opacity-90 pc:bg-orange-300 pc:text-white">
+      <div className="flex h-[52px] w-[120px] items-center justify-center rounded-lg text-lg text-orange-300 hover:opacity-90 pc:bg-orange-300 pc:text-white">
         <button>로그인</button>
       </div>
     </Link>
