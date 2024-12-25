f6d4f999982f8d233789b054d88ac75504e0b465
JSM <venise5224@gmail.com>
2024-12-25 14:38:55 +0900

    Chore:이미지 있을때만 보이게 수정

--- src/components/PostCardThumbnail.tsx
+++ src/components/PostCardThumbnail.tsx
@@ -3,12 +3,9 @@ import Image from "next/image";
 const PostCardThumbnail = ({ imageUrl }: { imageUrl: string }) => {
   return (
     <div className="relative h-[100px] w-[100px] overflow-hidden rounded-md pc:h-[150px] pc:w-[150px]">
-      <Image
-        src={imageUrl ? imageUrl : "/icon/profile-lg.svg"}
-        fill
-        className="object-cover"
-        alt="썸네일"
-      />
+      {imageUrl && (
+        <Image src={imageUrl} fill className="object-cover" alt="썸네일" />
+      )}
     </div>
   );
 };
