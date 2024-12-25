aff98c946e56d0bc9e0a0e1858fe9deed3093d8e
JSM <venise5224@gmail.com>
2024-12-25 14:26:01 +0900

    Chore: 케밥버튼 삭제 및 게시글 태블릿 사이즈 변경

--- src/components/card/PostCard.tsx
+++ src/components/card/PostCard.tsx
@@ -2,22 +2,19 @@
 
 import { useState } from "react";
 import { formatDate } from "../../utils/formatDate";
-import Image from "next/image";
 import CommentAndLikeCount from "../CommentAndLikeCount";
 import UserInfoInCard from "../UserInfoInCard";
 import { PostCardProps } from "@/types/post";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const PostCard = ({ info }: { info: PostCardProps }) => {
   const [isHover, setIsHover] = useState(false);
   const [_, formattedCreatedDate] = formatDate("", info.createdAt);
-  const pathname = usePathname();
 
   return (
     <Link href={`albatalks/${info.id}`}>
       <section
-        className="relative flex h-[210px] w-[327px] flex-col justify-between rounded-[16px] border border-line-100 bg-gray-50 p-6 shadow-md hover:bg-oldLace-50 pc:h-[280px] pc:w-[477px] tablet:h-[180px] tablet:w-[600px]"
+        className="relative flex h-[210px] w-[327px] flex-col justify-between rounded-[16px] border border-line-100 bg-gray-50 p-6 shadow-md hover:bg-oldLace-50 pc:h-[280px] pc:w-[477px] tablet:h-[180px] tablet:w-full"
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
       >
@@ -39,11 +36,6 @@ const PostCard = ({ info }: { info: PostCardProps }) => {
             isHover={isHover}
           />
         </div>
-        {pathname !== "/albatalk" && (
-          <button className="absolute right-6 top-6">
-            <Image src="/icon/kebab-md.svg" width={24} height={24} alt="메뉴" />
-          </button>
-        )}
       </section>
     </Link>
   );
