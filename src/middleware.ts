2affae8dd1ef62cb2676c4ebebdcc978c091107a
byeolee1221 <zicsso1221@gmail.com>
2024-12-25 17:29:05 +0900

    Feat: 미들웨어 보완하여 리다이렉트 시 토스트 띄우도록 하는 기능 및 컴포넌트 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,14 @@ export const middleware = async (request: NextRequest) => {
   const path = url.pathname;
   const role = request.cookies.get("role");
   const referer = request.headers.get("referer");
-  const ownerPath = [
-    "/owner",
-    "/addform",
-    "/addform/",
-    "/applications/",
+  const ownerPath = ["/owner", "/addform", "/addform/", "/applications/"];
+  const applicantPath = ["/applicant", "/apply/"];
+  const allAuthPath = [
+    ...ownerPath,
+    ...applicantPath,
+    "/mypage",
     "/myalbaform",
   ];
-  const applicantPath = ["/applicant", "/apply/", "/myapply/", "/mypage"];
   const accessToken = request.cookies.get("accessToken");
 
   // 액세스토큰 만료 시 리프레시토큰으로 액세스토큰 재발급 후 요청 재전송
@@ -45,41 +45,45 @@ export const middleware = async (request: NextRequest) => {
     }
   }
 
+  // searchParams 설정
+  const redirectSearchParams = (prevUrl: string | URL) => {
+    const errorUrl = new URL(prevUrl);
+
+    if (!role && !accessToken) {
+      errorUrl.searchParams.set("nonMemberError", "signinError");
+    } else {
+      errorUrl.searchParams.set("roleError", "roleError");
+    }
+    return errorUrl;
+  };
+
   // 사장님용 페이지
   if (role && role.value === "APPLICANT" && ownerPath.includes(path)) {
-    // const prevUrl = referer || url.origin;
-    // const errorUrl = new URL(prevUrl);
-    // errorUrl.searchParams.set("error", "matchErrror");
-
-    if (referer) {
-      return NextResponse.redirect(referer);
-    } else {
-      return NextResponse.redirect(url.origin);
-    }
+    return NextResponse.redirect(redirectSearchParams(referer || url.origin));
   }
 
   // 지원자용 페이지
   if (role && role.value === "OWNER" && applicantPath.includes(path)) {
-    // const prevUrl = referer || url.origin;
-    // const errorUrl = new URL(prevUrl);
-    // errorUrl.searchParams.set("error", "matchErrror");
-
-    if (referer) {
-      return NextResponse.redirect(referer);
-    } else {
-      return NextResponse.redirect(url.origin);
-    }
+    return NextResponse.redirect(redirectSearchParams(referer || url.origin));
   }
 
   // 비회원이 전용페이지 접근하는 것을 차단
   if (!role && !accessToken) {
-    const isOwnerPath = ownerPath.some((path) => url.pathname.startsWith(path));
-    const isApplicantPath = applicantPath.some((path) =>
-      url.pathname.startsWith(path)
-    );
-    if (isOwnerPath || isApplicantPath) {
-      return NextResponse.redirect(new URL("/signin/applicant", request.url));
+    if (path.startsWith("/signin")) {
+      return NextResponse.next();
     }
+
+    if (allAuthPath.some((authPath) => path.startsWith(authPath))) {
+      if (ownerPath.some((ownerRoute) => path.startsWith(ownerRoute))) {
+        return NextResponse.redirect(
+          redirectSearchParams(new URL("/signin/owner", request.url))
+        );
+      }
+      return NextResponse.redirect(
+        redirectSearchParams(new URL("/signin/applicant", request.url))
+      );
+    }
+    return NextResponse.next();
   }
 
   // 로그인되어 있으면 로그인페이지로 못가도록 막음
