a4cd68191decbf4c441ca50f7629af2453554767
JSM <JSM@>
2025-07-22 02:05:35 +0900

    feature/간편로그인후토스트

--- src/lib/OAuthLoginCheck.tsx
+++ src/lib/OAuthLoginCheck.tsx
@@ -3,16 +3,20 @@
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
+import { isLoggedAtom } from "@/atoms/isLogged";
+import { useSetAtom } from "jotai";
 
 const OAuthLoginCheck = () => {
   const pathname = usePathname();
   const id = Cookies.get("id");
+  const isLogged = useSetAtom(isLoggedAtom);
 
   useEffect(() => {
     if (pathname.startsWith("/") && id) {
       localStorage.setItem("isLogin", "true");
+      isLogged(true);
     }
-  }, [pathname, id]);
+  }, [pathname, id, isLogged]);
 
   return null;
 };
